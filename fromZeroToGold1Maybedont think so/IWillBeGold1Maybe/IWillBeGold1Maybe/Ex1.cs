7a6b7212696daa2a4ba424f143b4c1f2c114b241
HASH1MX <hashim8work@gmail.com>
2025-01-04 15:37:43 +0100

    Exercise 1 Done

---
+++ fromZeroToGold1Maybedont think so/IWillBeGold1Maybe/IWillBeGold1Maybe/Ex1.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace MyApplication
+{
+	class Ex1
+	{
+		public static void Run()
+		{
+			Console.Write("Hello");
+            Console.WriteLine(" Hashim Waleed");
+        }
+	}
+}
